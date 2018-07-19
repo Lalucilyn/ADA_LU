f6d99fcdc6deda3836480b31aa89dfb32492321a
Lucía Wainfeld <luwainfeld@gmail.com>
2018-07-19 20:26:05 -0300

    commit correctivo porque no se me está pusheando el server

--- EJERCITACION/TRABAJOS_PRÁCTICOS/TpFinal/servidor/routes/items.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var cors = require('cors');
-var busquedaController = require('../controllers/controllerCajaBusqueda');
-var axios = require('axios')
-/* GET users listing. */
-router.get('/', busquedaController.apiBusqueda)
-router.get('/item', busquedaController.apiProducto)
-module.exports = router;
